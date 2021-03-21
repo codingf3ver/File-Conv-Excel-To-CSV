fd4dc822eb5b8c858db278f45e11cbfdac2e9106
Md Tausif <codingf3ver@gmail.com>
2021-03-21 18:00:21 +0530

    file conversion uploaded

---
+++ excel_to_csv.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import pandas as pd
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue2', relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='File Conversion Tool', bg = 'lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 60, window=label1)
+
+def getExcel ():
+    global read_file
+    
+    import_file_path = filedialog.askopenfilename()
+    read_file = pd.read_excel (import_file_path)
+    
+browseButton_Excel = tk.Button(text="      Import Excel File     ", command=getExcel, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 130, window=browseButton_Excel)
+
+def convertToCSV ():
+    global read_file
+    
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
+    read_file.to_csv (export_file_path, index = None, header=True)
+
+saveAsButton_CSV = tk.Button(text='Convert Excel to CSV', command=convertToCSV, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 180, window=saveAsButton_CSV)
+
+def exitApplication():
+    MsgBox = tk.messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
+    if MsgBox == 'yes':
+       root.destroy()
+     
+exitButton = tk.Button (root, text='       Exit Application     ',command=exitApplication, bg='brown', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 230, window=exitButton)
+
+root.mainloop()
+
+
+    
